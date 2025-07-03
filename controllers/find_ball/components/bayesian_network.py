a7e8d667d197162a45f5f5070311c183f7599a77
enzoMagalhaes <magalhaes.enzo82@gmail.com>
2025-07-02 22:04:58 -0300

    building bayesian network probability table

--- controllers/find_ball/components/bayesian_network.py
+++ controllers/find_ball/components/bayesian_network.py
@@ -3,38 +3,50 @@
 YELLOW_MAX_COLOR_DISTANCE = 100
 
 class BayesianNetwork:
-    # left = -1 , forward = 0, right = 1
     def __init__(self):
-        # (obstacle_detected, target_visible, direction, action) -> success
-        self.success_cpt = {}
-        for obstacle_detected in (True, False):
-            for target_visible in (True, False):
-                for direction in range(-1,2):
-                    for action in range(-1,2):
-                        prob = 0.5
+        self.success_cpt = {
+            (True, True, -1, -1): 0.9,
+            (True, True, -1, 0): 0.5,
+            (True, True, -1, 1): 0.1,
+            (True, True, 0, -1): 0.5,
+            (True, True, 0, 0): 0.9,
+            (True, True, 0, 1): 0.5,
+            (True, True, 1, -1): 0.1,
+            (True, True, 1, 0): 0.5,
+            (True, True, 1, 1): 0.9,
+            (True, False, -1, -1): 0.1,
+            (True, False, -1, 0): 0.5,
+            (True, False, -1, 1): 0.8,
+            (True, False, 0, -1): 0.5,
+            (True, False, 0, 0): 0.1,
+            (True, False, 0, 1): 0.5,
+            (True, False, 1, -1): 0.8,
+            (True, False, 1, 0): 0.5,
+            (True, False, 1, 1): 0.1,
+            (False, True, -1, -1): 0.5,
+            (False, True, -1, 0): 0.7,
+            (False, True, -1, 1): 0.5,
+            (False, True, 0, -1): 0.5,
+            (False, True, 0, 0): 0.7,
+            (False, True, 0, 1): 0.5,
+            (False, True, 1, -1): 0.5,
+            (False, True, 1, 0): 0.7,
+            (False, True, 1, 1): 0.5,
+            (False, False, -1, -1): 0.5,
+            (False, False, -1, 0): 0.7,
+            (False, False, -1, 1): 0.5,
+            (False, False, 0, -1): 0.5,
+            (False, False, 0, 0): 0.7,
+            (False, False, 0, 1): 0.5,
+            (False, False, 1, -1): 0.5,
+            (False, False, 1, 0): 0.7,
+            (False, False, 1, 1): 0.5,
+        }
 
-                        # if obstacle was detected and it is not the target, go to the opposite direction
-                        if obstacle_detected:
-                            if not target_visible:
-                                if direction == action:
-                                    prob = 0.1
-                                elif direction == -action:
-                                    prob = 0.8
-                            else:
-                                if direction == action:
-                                    prob = 0.9
-                                elif direction == -action:
-                                    prob = 0.1
-                        else:
-                            if action == 0:
-                                prob = 0.7
-
-                        self.success_cpt[(obstacle_detected, target_visible, direction, action)] = prob
-                                
     def obstacle_detected(self, dist):
         return dist < 0.4
 
-    def direction(self,angle):
+    def direction(self, angle):
         if angle <= 30:
             return -1
         elif angle <= 60:
@@ -42,17 +54,19 @@ class BayesianNetwork:
         else:
             return 1
 
-    def target_visible(self,is_yellow):
+    def target_visible(self, is_yellow):
         return is_yellow < YELLOW_MAX_COLOR_DISTANCE
-    
-    def next_action(self, dist,angle,is_yellow):
+
+    def next_action(self, dist, angle, is_yellow):
         obstacle_detected = self.obstacle_detected(dist)
         direction = self.direction(angle)
         target_visible = self.target_visible(is_yellow)
-        
+
         mx_action = [None, 0]
-        for action in range(-1,2):
-            success_probability = self.success_cpt[(obstacle_detected, target_visible, direction, action)]
+        for action in range(-1, 2):
+            success_probability = self.success_cpt[
+                (obstacle_detected, target_visible, direction, action)
+            ]
             if success_probability > mx_action[1]:
                 mx_action = [action, success_probability]
         return mx_action[0]
--- controllers/find_ball/find_ball_controller.py
+++ controllers/find_ball/find_ball_controller.py
@@ -7,32 +7,25 @@ from components.cnn import get_values_from_cnn
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
-camera = robot.getDevice("camera") 
+camera = robot.getDevice("camera")
 camera.enable(timestep)
 
-lidar = robot.getDevice("lidar")  
+lidar = robot.getDevice("lidar")
 lidar.enable(timestep)
-    
-robot_controller = RobotActionController(robot,timestep)
+
+robot_controller = RobotActionController(robot, timestep)
 bayesian_network = BayesianNetwork()
 
 while robot.step(timestep) != -1:
     img = camera.getImageArray()
     lidar_ranges = lidar.getRangeImage()
 
-    dist, angle, yellow_distance = get_values_from_cnn(img,lidar_ranges)
+    dist, angle, yellow_distance = get_values_from_cnn(img, lidar_ranges)
 
     if yellow_distance < YELLOW_MAX_COLOR_DISTANCE and dist < 0.3:
         break
 
-    next_action = bayesian_network.next_action(dist,angle,yellow_distance)
+    next_action = bayesian_network.next_action(dist, angle, yellow_distance)
     robot_controller.perform_action(next_action)
 
-
     print(next_action)
-
-
-
-    
-    
-    
